6ef150de00cd4e3f03c3e8c8fee1daf41c4aee96
David Bitar <61165835+davidj20012@users.noreply.github.com>
2022-10-30 16:55:15 -0400

    Update UserQueue.h

--- PA_02/UserQueue.h
+++ PA_02/UserQueue.h
@@ -4,10 +4,39 @@
 
 #ifndef PA_02_USERQUEUE_H
 #define PA_02_USERQUEUE_H
+#include <string>
+#include "User.h"
+
+using namespace std;
 
 
-class UserQueue {
+class UserQueue{
+private:
+    User *head;
+    User *tail;
 
+
+public:
+    UserQueue();
+    //~UserQueue();
+
+    //Get functions
+    User *getHead();
+    User *getTail();
+
+    //Creates user at end
+    void createUserEnd(int);
+
+    //Removes head from queue
+    void removefromqueue();
+
+    //Cycles head to back of queue
+    void cycleuser();
+
+    //Testing functions
+    void nextinqueue();
+    void valueofNodeNposition(int pos);
+    friend std::ostream & operator<<(ostream&, UserQueue&);
 };
 
 
