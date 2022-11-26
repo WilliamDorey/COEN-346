c2790851b08e0ec74b70278788fcf820fabe6325
William Dorey <43609990+WilliamDorey@users.noreply.github.com>
2022-11-26 16:43:07 -0500

    initial commit Assignment 3

---
+++ Assignment_03/MemorySpace.h
@@ -0,0 +1,32 @@
+//
+// Created by willi on 2022-11-21.
+//
+
+#ifndef ASSIGNMENT_03_MEMORYSPACE_H
+#define ASSIGNMENT_03_MEMORYSPACE_H
+
+#include "MemoryPage.h"
+#include <fstream>
+
+using namespace std;
+
+class MemorySpace {
+private:
+    MemoryPage* MainMem{};
+    int size{};
+    fstream disk;
+
+public:
+    MemorySpace();
+    explicit MemorySpace(int);
+
+    ~MemorySpace();
+
+    void Store(string, unsigned int);
+    void Release(string);
+    int Lookup(string);
+
+};
+
+
+#endif //ASSIGNMENT_03_MEMORYSPACE_H
