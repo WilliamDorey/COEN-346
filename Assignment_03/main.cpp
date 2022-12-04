07f229c3f1d41d44105e338d0736d081fa309b5d
NajeebH <90568747+NajeebH@users.noreply.github.com>
2022-12-03 23:19:15 -0500

    Adding Process thread func and VMM thread func

--- Assignment_03/main.cpp
+++ Assignment_03/main.cpp
@@ -6,13 +6,16 @@
 #include <random>
 #include <fstream>
 #include <semaphore>
+#include <mutex>
 #include "processQueue.h"
+#include "MemorySpace.h"
 
 // Declaring default namespace for code clarity
 using namespace std;
 
 //Global variables for Semaphores
 counting_semaphore cores{0};
+counting_semaphore vm_turn{0};
 
 // TODO: Separate functions into respective .cpp files for conciseness and modularity?
 //  Scheduler + Process
@@ -23,9 +26,10 @@ counting_semaphore cores{0};
 // Declaring functions
 // TODO: Decide which functions can open a stream vs just have it passed to if by reference
 [[noreturn]] void virtual_clock(int&);
-void process(int&, fstream&, fstream&, int,int);
+void process(int&, fstream&, fstream&, int, int, int);
 void scheduler(int&, fstream&);
 int getRandom();
+void VMmanager(int&, int&, fstream&, fstream&, int);
 
 // Main Driver function
 int main() {
@@ -84,7 +88,7 @@ void scheduler(int& clk, fstream& logs){
         info = queue->getInfo();
         while( (clk / 1000 ) < info[0]);
         cores.acquire();
-        thread thr_proc(process, ref(clk), ref(commands), ref(logs), info[1], info[2]);
+        thread thr_proc(process, ref(clk), ref(commands), ref(logs), info[0], info[1], info[2]);
         thr_proc.detach();
         if (queue->nextProc() == -1)
             break;
@@ -101,12 +105,10 @@ void scheduler(int& clk, fstream& logs){
 
 // Process function
 // Only should be called by scheduler when it has decided to start the process
-void process(int& clk, fstream& commands, fstream& log, int burst, int ID){
+void process(int& clk, fstream& commands, fstream& log, int start, int burst, int ID){
     int sClk = clk;
     cout << "Starting process " << ID << " at clock time " << clk << endl;
-    while (burst > (clk - sClk)/1000);
-    cout << "Ending process " << ID << " at clock time " << clk << endl;
-    cores.release();
+
     // TODO: Log start and finish of process
 
     // TODO: Run the following loop until time to end
@@ -114,12 +116,57 @@ void process(int& clk, fstream& commands, fstream& log, int burst, int ID){
     //  wait for that time in milliseconds (or wait until end of burst) -> repeat
 
     // TODO: Signal semaphore at exit so next process can start
+
+    int API_time = 0;
+    string cmd;
+
+    while (burst*10 > API_time){
+        API_time += getRandom();
+        sClk += API_time;
+        thread thr_vmm(VMmanager, ref(sClk), ref(ID), ref(commands), ref(log), start);
+        thr_vmm.join();
+    }
+
+    while (burst > (clk - sClk)/1000);
+    cout << "Ending process " << ID << " at clock time " << clk << endl;
+    cores.release();
+}
+
+void VMmanager(int& sClk, int& ID, fstream& commands, fstream& log,int arrival){
+    string cmd;
+    commands >> cmd;
+    int vID, val;
+
+    MemorySpace* mem;
+
+    //need flag; if not my turn, stuck in while loop
+    //if arr => sClk, exit while loop
+    //while (arrival*1000 < sClk);
+    if(cmd == "Store"){
+        commands >> vID;
+        commands >> val;
+       // mem->Store(to_string(vID),val);
+        log << "Clock: " << sClk << " Process: " << ID << ", Store: Variable " << vID << ", Value: " << val << endl;
+    }
+    else if (cmd == "Lookup"){
+        commands >> vID;
+        //mem->Lookup(to_string(vID));
+        log << "Clock: " << sClk << " Process: " << ID << ", Lookup: Variable " << vID << endl;
+    }
+    else if (cmd == "Release"){
+        commands >> vID;
+        //mem->Release(to_string(vID));
+        log << "Clock: " << sClk << " Process: " << ID << ", Release: Variable " << vID << endl;
+    }
+    else
+        cout << "Invalid Command in input file\n";
+
 }
 
 // Function to return a random integer between 10 and 1000
 int getRandom(){
     random_device device;
     mt19937 generator(device());
-    uniform_int_distribution<int> dist(1,100);
+    uniform_int_distribution<int> dist(1,1);
     return dist(generator) * 10;
 }
