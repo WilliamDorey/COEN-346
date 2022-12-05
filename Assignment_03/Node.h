1fa0264891f54c91f61fb221ae1d4d9594a705a8
David Bitar <61165835+davidj20012@users.noreply.github.com>
2022-12-04 22:37:58 -0500

    Create Node.h

---
+++ Assignment_03/Node.h
@@ -0,0 +1,34 @@
+//
+// Created by david on 2022-12-03.
+//
+
+
+#ifndef UNTITLED5_NODE_H
+#define UNTITLED5_NODE_H
+
+#include <string>
+using namespace std;
+
+class Node{
+private:
+    //Data types
+    Node *next;
+    string NodeID;
+public:
+    //Constructors and Destructor
+    Node();
+    Node(string);
+    ~Node();
+
+    //Get functions
+    Node *getNext();
+    Node *getPrevious();
+    string getID();
+
+    //Set functions
+    void setNexttoNULL();
+    void setNext(Node *);
+    void setID(string);
+};
+
+#endif //UNTITLED5_NODE_H
