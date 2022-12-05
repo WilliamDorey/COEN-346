77e562e8782f05dd74b963ee9c12e3a01fee8e51
NajeebH <90568747+NajeebH@users.noreply.github.com>
2022-12-04 22:45:42 -0500

    working code

--- Assignment_03/main.cpp
+++ Assignment_03/main.cpp
@@ -15,16 +15,16 @@ using namespace std;
 
 //Global variables for Semaphores
 counting_semaphore cores{0};
-counting_semaphore vm_turn{0};
-bool flag = false;
+binary_semaphore vm_turn{1};
+bool flag = true;
 
 // Declaring functions
 // TODO: Decide which functions can open a stream vs just have it passed to if by reference
 [[noreturn]] void virtual_clock(int&);
-void process(int&, fstream&, fstream&, int, int, int);
+void process(int&, fstream&, fstream&, int, int);
 void scheduler(int&, fstream&);
 int getRandom();
-void VM_manager(int&, int&, int&, fstream&, fstream&, int);
+void VM_manager(int, int, fstream&, fstream&);
 void setFlag(int);
 
 // Main Driver function
@@ -84,7 +84,7 @@ void scheduler(int& clk, fstream& logs){
         info = queue->getInfo();
         while( (clk / 1000 ) < info[0]);
         cores.acquire();
-        thread thr_proc(process, ref(clk), ref(commands), ref(logs), info[0], info[1], info[2]);
+        thread thr_proc(process, ref(clk), ref(commands), ref(logs), info[1], info[2]);
         thr_proc.detach();
         if (queue->nextProc() == -1)
             break;
@@ -101,32 +101,35 @@ void scheduler(int& clk, fstream& logs){
 
 // Process function
 // Only should be called by scheduler when it has decided to start the process
-void process(int& clk, fstream& commands, fstream& log, int start, int burst, int ID){
+void process(int& clk, fstream& commands, fstream& log, int burst, int ID){
     int sClk = clk;
-    cout << "Starting process " << ID << " at clock time " << clk << endl;
-
-    if(clk == 1000)
-        setFlag(1);     //let first process go in
-    while(!flag); //busy wait until flag=1
-    setFlag(0);         //lock VMM; only 1 process accesses it at a time
-
+    int remain = burst*1000;
     int API_time = 0;
-    string cmd;
+    int clk2 = clk;
+    log << "Clock: " << clk << ", Process: " << ID << ": Started." << endl;
 
-    while (burst*10 > API_time){
+    while(!flag);              //busy wait until flag=1;
+
+    do{
+        setFlag(0);                 //lock VMM; only 1 process accesses it at a time
         API_time += getRandom();
         sClk += API_time;
-        thread thr_vmm(VM_manager, ref(clk), ref(sClk), ref(ID), ref(commands), ref(log), start);
+        while(clk < sClk);                  //wait until API finishes
+        remain = remain - API_time;
+        if(remain < 0)
+            break;
+        thread thr_vmm(VM_manager, sClk, ID, ref(commands), ref(log));  //execute API calls in a thread
         thr_vmm.join();
-    }
+        setFlag(1);                  //release for another API call to go through
+    } while (remain > API_time);
 
-    while (burst > (clk - sClk)/1000);
-    cout << "Ending process " << ID << " at clock time " << clk << endl;
-    setFlag(1); //release it
+    while (burst > (clk - clk2)/1000);
+    log << "Clock: " << clk << ", Process: " << ID << ": Finished." << endl;
     cores.release();
 }
 
-void VM_manager(int& clk, int& sClk, int& ID, fstream& commands, fstream& log,int arrival){
+//Function to execute API calls based on commands.txt input file
+void VM_manager(int sClk, int ID, fstream& commands, fstream& log){
     string cmd;
     commands >> cmd;
     int vID, val;
@@ -137,32 +140,32 @@ void VM_manager(int& clk, int& sClk, int& ID, fstream& commands, fstream& log,in
         commands >> vID;
         commands >> val;
         // mem->Store(to_string(vID),val);
-        log << "Clock: " << sClk << " Process: " << ID << ", Store: Variable " << vID << ", Value: " << val << endl;
+        log << "Clock: " << sClk << ", Process: " << ID << ", Store: Variable " << vID << ", Value: " << val << endl;
     }
     else if (cmd == "Lookup"){
         commands >> vID;
         //mem->Lookup(to_string(vID));
-        log << "Clock: " << sClk << " Process: " << ID << ", Lookup: Variable " << vID << endl;
+        log << "Clock: " << sClk << ", Process: " << ID << ", Lookup: Variable " << vID << endl;
     }
     else if (cmd == "Release"){
         commands >> vID;
         //mem->Release(to_string(vID));
-        log << "Clock: " << sClk << " Process: " << ID << ", Release: Variable " << vID << endl;
+        log << "Clock: " << sClk << ", Process: " << ID << ", Release: Variable " << vID << endl;
     }
-    else
-        cout << "Invalid Command in input file\n";
+    else{
+        commands.clear();
+        commands.seekg(0, ios::beg);        //start reading file over when it's done
+    }
 }
 
 void setFlag(int flagVal){
-//    vm_turn.acquire();
     flag = flagVal;
-//    vm_turn.release();
 }
 
 // Function to return a random integer between 10 and 1000
 int getRandom(){
     random_device device;
     mt19937 generator(device());
-    uniform_int_distribution<int> dist(1,1);
+    uniform_int_distribution<int> dist(1,100);
     return dist(generator) * 10;
 }
